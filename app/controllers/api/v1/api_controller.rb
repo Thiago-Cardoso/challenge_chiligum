2a0fcaaee3ab3a7b5d70cb1196941cc0dff58d4b
Thiago-Cardoso <thiagocardosooti@gmail.com>
2020-11-29 11:34:07 -0200

    Logic to consume api rick and morty and return the json data response to the api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,11 +1,9 @@
 module Api
   module V1
     class ApiController < ApplicationController
-
       rescue_from ActionController::ParameterMissing do |exception|
         render(json: { message: exception.param }, status: :bad_request)
       end
-
     end
   end
 end
--- app/controllers/api/v1/informations_controller.rb
+++ app/controllers/api/v1/informations_controller.rb
@@ -1,5 +1,12 @@
-class Api::V1::InformationsController < ApplicationController
-  def show
-    
+module Api
+  module V1
+    class InformationsController < Api::V1::ApiController
+      def show
+        result = InformationService.new(params[:id]).call
+          render json: result
+          rescue => error
+            render json: { errors: "Error was not informed the character id"}, status: :unprocessable_entity
+          end
+      end
   end
 end
